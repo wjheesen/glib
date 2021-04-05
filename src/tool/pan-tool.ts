46577d7ee6ed439d583b6590decaf63efe72204f
William Heesen <wheesen@warwicksd.org>
2021-04-05 17:48:30 -0400

    Invalidate or reset panning event if more than one pointer on the screen

--- src/tool/pan-tool.ts
+++ src/tool/pan-tool.ts
@@ -2,17 +2,29 @@ import { Point, Pointer, PointerEventListener, Vec2 } from "..";
 
 export class PanTool extends PointerEventListener {
 
-    private previous: Point.Like;
+    private previous?: Point.Like;
 
     wherePointer(p: Pointer) {
-        return p.isDown && p.activePointers.length == 1;
+        return p.isDown;
     }
 
     onPointerDown(p: Pointer) {
-        this.previous = p.position;
+        if (p.activePointers.length == 1) {
+            this.previous = p.position;
+        }
     }
 
-    onPointerMove({position, surface}: Pointer) {
+    onPointerMove(p: Pointer) {
+        if (p.activePointers.length > 1) {
+            this.previous = null; // Invalidate
+        } else if (!this.previous) {
+            this.previous = p.position; // Reset
+        } else {
+            this.pan(p);
+        }
+    }
+
+    private pan({position, surface}: Pointer) {
         let toPrevious = Vec2.fromPointToPoint(position, this.previous); 
         let actual = surface.pan(toPrevious);
         Vec2.add(actual, position, this.previous);
